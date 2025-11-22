1c2716faea9996fb6a518e27721b533505b6cd43
joey-en <joyannpdelacruz@gmail.com>
2025-11-22 20:01:50 +0300

    Tested in main to see confirm new functions are working

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 from src.scene_cutting import get_scene_list
 from src.frame_sampling import sample_frames
 from src.frame_captioning_blip import caption_frames
-# from src.frame_captioning_heavy import refine_caption_frames   
 from src.debug_utils import *
 
-test_video = r'Videos\SpongeBob SquarePants - Writing Essay - Some of These - Meme Source.mp4'
+test_video = r"Videos\Spain Vlog.mp4"
 
-scenes = get_scene_list(test_video)
+scenes = get_scene_list(test_video, min_scene_sec=2) 
+scenes = save_clips(test_video, scenes, output_dir="./output/clips")
 see_scenes_cuts(scenes)
 
 scenes_with_frames = sample_frames(
     input_video_path=test_video,
     scenes=scenes,
     num_frames=3,
+    new_size = 320,
     output_dir="./output/frames",
 )
-# Has these errors (we need to check why):
-# [h264 @ 000002602f6b6600] mmco: unref short failure
-# [h264 @ 000002602fc03780] mmco: unref short failure
 
 captioned_scenes = caption_frames(
     scenes=scenes_with_frames,
@@ -28,19 +26,4 @@ captioned_scenes = caption_frames(
     prompt="a video frame of"
 )
 
-save_scenes_to_file(captioned_scenes, "output/captioned_scenes.json")
-
-# refined_scenes = refine_caption_frames(
-#     scenes=captioned_scenes,
-#     num_prev=1,
-#     num_next=1,
-#     extra_instruction=(
-#         "Using the image and these captions as temporal context, "
-#         "write ONE concise sentence describing what is happening "
-#         "in this frame, focusing on new details or clarifications."
-#     ),
-#     do_sample=False,
-#     debug=True,
-# )
-
-# see_first_scene(refined_scenes)
+save_vid_df(captioned_scenes, "output/captioned_scenes.json")
